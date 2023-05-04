c66ba6fec1a6d59371d7f6a6779732bb0eeb15c4
xieyz10 <54917731+xieyz10@users.noreply.github.com>
2023-05-04 14:24:15 -0400

    first commit

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,121 @@
+'use client'
+
+import MenuItem from './MenuItem';
+import { AiOutlineMenu } from 'react-icons/ai'
+import Avatar from '../Avatar'
+import { useCallback, useState } from 'react';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
+
+interface UserMenuProps {
+    currentUser?: SafeUser | null
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+    const resgiterModal = useRegisterModal();
+    const loginModal = useLoginModal()
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value)
+    }, [])
+    return (
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                <div
+                    onClick={() => { }}
+                    className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+                >
+                    Airbnb your home
+                </div>
+                <div
+                    onClick={toggleOpen}
+                    className="
+                        p-4
+                        md:py-2
+                        md:px-2
+                        border-[1px]
+                        border-neutral-200
+                        flex
+                        flex-row
+                        items-center
+                        gap-3
+                        rounded-full
+                        hover:shadow-md
+                        transition
+                        cursor-pointer
+                    "
+                >
+                    <AiOutlineMenu />
+                    <div className='hidden md:block'>
+                        <Avatar />
+                    </div>
+                </div>
+            </div>
+
+            {isOpen && (
+                <div
+                    className='
+                        absolute
+                        rounded-xl
+                        shadow-md
+                        w-[40vw]
+                        md:w-3/4
+                        bg-white
+                        overflow-hidden
+                        right-0
+                        top-12
+                        text-sm
+                    '
+                >
+                    <div className='flex flex-col cursor-pointer'>
+                        {currentUser ? (
+                            <>
+                                <MenuItem
+                                    onClick={()=>{}}
+                                    label="My trips"
+                                />
+                                <MenuItem
+                                    onClick={()=>{}}
+                                    label="My Favorite"
+                                />
+                                <MenuItem
+                                    onClick={()=>{}}
+                                    label="My Reservations"
+                                />
+                                <MenuItem
+                                    onClick={()=>{}}
+                                    label="My properties"
+                                />
+                                <MenuItem
+                                    onClick={()=>{}}
+                                    label="Airbnb my home"
+                                />
+                                <hr />
+                                <MenuItem
+                                    onClick={()=>signOut()}
+                                    label="Logout"
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <MenuItem
+                                    onClick={loginModal.onOpen}
+                                    label="Login"
+                                />
+                                <MenuItem
+                                    onClick={resgiterModal.onOpen}
+                                    label="Sign up"
+                                />
+                            </>
+                        )}
+                    </div>
+                </div>
+            )}
+
+        </div>
+    )
+}
+
+export default UserMenu
